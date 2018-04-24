9c41504dd3eca3b0fb3f92f60569efa08e5c1e55
Konstantin <KonstantinLeontev@users.noreply.github.com>
2018-04-24 15:51:14 +0300

    NetLevels header was added.

---
+++ NetDataParser/NetLevels.h
@@ -0,0 +1,39 @@
+// All structs describes the given net levels.
+
+struct Network_V1 {
+	// Version 1 byte.
+	char sourceAddress[4];
+	char destAddress[4];
+	char protocol; // 0x01 - TRANSPORT V1 or 0x02 - TRANSPORT V2
+	char dataSize[2];
+	char headerChecksum[2];
+	// Data 14-N.
+};
+
+struct Network_V2 {
+	// Version 1 byte.
+	char sourceAddress[6];
+	char destAddress[6];
+	char protocol; // 0x01 - TRANSPORT V1 or 0x02 - TRANSPORT V2
+	char dataSize[2];
+	char headerChecksum[2];
+	// Data 18-N.
+};
+
+struct Transport_V1 {
+	char sourcePort[2];
+	char destinPort[2];
+	char dataSize[2];
+	// Data - 6-N.
+	// Checksum - N + 1, N + 2.
+};
+
+struct Transport_V2 {
+	char sourcePort[2];
+	char destinPort[2];
+	char fragmentNumber[4];
+	char lf; // Last 2 bits: F - first packet in the session, L - last packet in the session.
+	char dataSize[2];
+	// Data - 11-N.
+	// Checksum - N + 1, N + 2.
+};
